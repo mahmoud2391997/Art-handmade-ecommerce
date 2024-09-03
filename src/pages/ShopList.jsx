20461c4b1ec7c7ab8e9ad6a35a52fbc55deb6f8b
marina-mansour <marinamansour400@gmail.com>
2024-09-03 05:06:06 +0300

    fetch categiries from database into filter section | filter by category |filter by price using debounce technique | search using debounce technique | sorting by 4 option | optimize performance using useMemo

--- src/pages/ShopList.jsx
+++ src/pages/ShopList.jsx
@@ -1,15 +1,19 @@
-import { Button, Option, Select, Typography } from "@material-tailwind/react";
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useDebounce } from "use-debounce";
+
+import { Option, Select, Typography } from "@material-tailwind/react";
+
+import { useSelector } from "react-redux";
+
+import { getCategories } from "../api/categories";
+
+import { debounce } from "../utils/debounce";
+
 import PageTitle from "../components/Shared/PageTitle";
 import SearchInput from "../components/SearchInput";
 import RangeSlider from "../components/RangeSlider";
 import CategoriesCheckbox from "../components/CategoriesCheckBox";
 import TagsFilter from "../components/TagsFilter";
-import ProductCard from "../components/CustomerProductCard";
-import { useSelector } from "react-redux";
-import SingleProductCard from "../components/Shared/SingleProductCard";
-import LeftIcon from "../components/icons/LeftIcon";
-import RightIcon from "../components/icons/RightIcon";
 import ProductList from "../components/ProductListFinal";
 import Pagination from "../components/Shared/Pagination";
 
@@ -17,12 +21,68 @@ export default function ShopList() {
   const { products } = useSelector((state) => state.products);
   console.log(products);
 
+  const [categories, setCategories] = useState([]);
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [value, setValue] = useState(1500);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortOption, setSortOption] = useState("default");
+  const [debouncedValue] = useDebounce(value, 300);
+  const [debouncedSearchTerm] = useDebounce(searchTerm, 500);
+
+  //no use now but will be needed
+  const [minValue, setMinValue] = useState(50);
+  const [maxValue, setMaxValue] = useState(1500);
+
+  const filteredProducts = useMemo(() => {
+    let sortedProducts = products.filter((product) => {
+      const matchesCategory =
+        selectedCategories.length > 0
+          ? selectedCategories.some(
+              (selectedCategory) => selectedCategory._id === product.categoryId
+            )
+          : true;
+
+      const matchesPrice = product.price <= debouncedValue;
+      // const matchesPrice = product.price >= minValue && product.price <= maxValue;
+
+      const matchesSearch = product.name
+        .toLowerCase()
+        .includes(debouncedSearchTerm.toLowerCase());
+
+      return matchesCategory && matchesPrice && matchesSearch;
+    });
+
+    if (sortOption === "priceLowToHigh") {
+      sortedProducts = sortedProducts.sort((a, b) => a.price - b.price);
+    } else if (sortOption === "priceHighToLow") {
+      sortedProducts = sortedProducts.sort((a, b) => b.price - a.price);
+    } else if (sortOption === "nameAZ") {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      );
+    } else if (sortOption === "nameZA") {
+      sortedProducts = sortedProducts.sort((a, b) =>
+        b.name.localeCompare(a.name)
+      );
+    }
+
+    return sortedProducts;
+  }, [
+    products,
+    selectedCategories,
+    debouncedValue,
+    debouncedSearchTerm,
+    sortOption,
+  ]);
+
+  console.log(filteredProducts);
+
   const productsPerPage = 6; // Number of products per page
   const [currentPage, setCurrentPage] = useState(1);
 
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = products.slice(
+  const currentProducts = filteredProducts.slice(
     indexOfFirstProduct,
     indexOfLastProduct
   );
@@ -31,7 +91,21 @@ export default function ShopList() {
     setCurrentPage(pageNumber);
   };
 
-  const totalPages = Math.ceil(products.length / productsPerPage);
+  const totalPages = Math.ceil(filteredProducts.length / productsPerPage);
+
+  const handleSearchChange = debounce((term) => {
+    setSearchTerm(term);
+    setCurrentPage(1);
+  }, 300);
+
+  const handleSortChange = (option) => {
+    setSortOption(option);
+    setCurrentPage(1);
+  };
+
+  useEffect(() => {
+    getCategories({ setCategories });
+  }, []);
 
   return (
     <div className="z-40  relative bg-white">
@@ -44,27 +118,25 @@ export default function ShopList() {
               style={{ fontFamily: "var(--third-font)" }}
             >
               showing {indexOfFirstProduct + 1} -
-              {Math.min(indexOfLastProduct, products.length)} of{" "}
-              {products.length} results
-              {/* showing 1-12 of {products.length} */}
+              {Math.min(indexOfLastProduct, filteredProducts.length)} of{" "}
+              {filteredProducts.length} results
             </div>
             <div>
-              <Select variant="standard" label="Sort">
-                <Option>price: low to high</Option>
-                <Option>price: high to low</Option>
-                <Option>a-z</Option>
-                <Option>z-a</Option>
+              <Select
+                variant="standard"
+                label="Sort"
+                on
+                onChange={(e) => handleSortChange(e)}
+              >
+                <Option value="default">default</Option>
+                <Option value="priceLowToHigh">price: low to high</Option>
+                <Option value="priceHighToLow">price: high to low</Option>
+                <Option value="nameAZ">a-z</Option>
+                <Option value="nameZA">z-a</Option>
               </Select>
             </div>
           </div>
           <div className="p-10 min-h-fit">
-            {/* {currentProducts.map((product, index) => (
-              <div key={index}>
-                <SingleProductCard prod={product} isRandom={false} />
-              </div>
-            ))} */}
-            {/* Pass isRandom as needed */}
-            {/* <ProductCard isRandom={false} /> */}
             <ProductList isRandom={false} currentProducts={currentProducts} />
           </div>
           <div className="flex justify-center items-start mt-4">
@@ -74,7 +146,59 @@ export default function ShopList() {
               currentPage={currentPage}
               handlePageChange={handlePageChange}
             />
-            {/* <Button
+          </div>
+        </div>
+        <div className="w-1/3 flex flex-col gap-4">
+          <SearchInput onChange={(e) => handleSearchChange(e.target.value)} />
+          <Typography
+            className="uppercase text-[22px] text-[var(--main-gray)] -mb-4"
+            style={{
+              fontFamily: "var(--main-font)",
+              letterSpacing: ".16em",
+              lineHeight: "1.31em",
+            }}
+          >
+            filter
+          </Typography>
+          <div className="w-full h-5 border-l-[1px] border-r-[1px] border-[var(--main-gray)] flex justify-center items-center mb-4">
+            <div className="w-full h-[1px] bg-[var(--main-gray)]" />
+          </div>
+          <RangeSlider
+            value={value}
+            setValue={setValue}
+            minValue={minValue}
+            maxValue={maxValue}
+            setMinValue={setMinValue}
+            setMaxValue={setMaxValue}
+          />
+          <CategoriesCheckbox
+            selectedCategories={selectedCategories}
+            setSelectedCategories={setSelectedCategories}
+            categories={categories}
+          />
+          {/* <TagsFilter /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* {currentProducts.map((product, index) => (
+              <div key={index}>
+                <SingleProductCard prod={product} isRandom={false} />
+              </div>
+            ))} */
+}
+{
+  /* Pass isRandom as needed */
+}
+{
+  /* <ProductCard isRandom={false} /> */
+}
+
+{
+  /* <Button
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}
               className="px-2 bg-transparent shadow-none hover:shadow-none transition-all duration-500 ease-in-out text-[var(--main-gray)] hover:text-[var(--main-color)]"
@@ -100,29 +224,5 @@ export default function ShopList() {
               className="px-2 bg-transparent shadow-none hover:shadow-none transition-all duration-500 ease-in-out text-[var(--main-gray)] hover:text-[var(--main-color)]"
             >
               <RightIcon />
-            </Button> */}
-          </div>
-        </div>
-        <div className="w-1/3 flex flex-col">
-          <Typography
-            className="uppercase text-[22px] text-[var(--main-gray)] mb-4"
-            style={{
-              fontFamily: "var(--main-font)",
-              letterSpacing: ".16em",
-              lineHeight: "1.31em",
-            }}
-          >
-            filter
-          </Typography>
-          <div className="w-full h-5 border-l-[1px] border-r-[1px] border-[var(--main-gray)] flex justify-center items-center mb-10">
-            <div className="w-full h-[1px] bg-[var(--main-gray)]" />
-          </div>
-          <RangeSlider />
-          <CategoriesCheckbox />
-          <TagsFilter />
-          <SearchInput />
-        </div>
-      </div>
-    </div>
-  );
+            </Button> */
 }
