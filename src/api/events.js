387a6fb3d22d294d8ead36fd67f647c3591039ed
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-09-07 22:04:07 +0300

    single event api done

--- src/api/events.js
+++ src/api/events.js
@@ -6,3 +6,11 @@ function getEvents() {
       console.error(error);
     });
 }
+function getEvents(eventId) {
+  axios
+    .get(`https://art-ecommerce-server.glitch.me/api/events/${eventId}`)
+    .then((response) => console.log(response.data))
+    .catch((error) => {
+      console.error(error);
+    });
+}
