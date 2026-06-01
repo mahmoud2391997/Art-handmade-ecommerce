ff138c62859ce8976abd07289ad8f76550e50425
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2026-06-01 08:39:32 +0300

    sadasd

--- src/components/icons/starIcon.jsx
+++ src/components/icons/starIcon.jsx
@@ -8,7 +8,7 @@ export default function StarIcon({ filled }) {
       viewBox="0 0 24 24"
       strokeWidth={1.5}
       stroke="var(--main-color)"
-      className="size-4"
+      className="size-5"
     >
       <path
         strokeLinecap="round"
